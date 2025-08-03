3387badfe53cffe3c7d6a169fcc76eefbcf0350e
Mehdi Dinari <mehdi.dinari@usmba.ac.ma>
2025-08-03 11:06:13 +0100

    8-3

--- Front/src/pages/reservations.js
+++ Front/src/pages/reservations.js
@@ -48,7 +48,7 @@ function Reservations() {
 
   const handleLogout = () => {
     localStorage.removeItem('token');
-    alert('Déconnexion réussie.');
+    alert('Disconnected.');
     navigate('/login');
   };
 
